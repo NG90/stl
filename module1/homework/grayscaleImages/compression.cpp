40a4963cc8ede4e869b2baf5c1caea125956e15f
kyrtaq <kyrtaq13@gmail.com>
2020-06-10 16:26:29 +0200

    greyscale completely redesigned

--- module1/homework/grayscaleImages/compression.cpp
+++ module1/homework/grayscaleImages/compression.cpp
@@ -1,1 +1,19 @@
-#include "grayscale.hpp"
+#include "compression.hpp"
+
+std::vector<std::pair<uint8_t, uint8_t>> compressGrayscale(
+    const std::array<std::array<uint8_t, width>, height>& bitmap) {
+    std::vector<std::pair<uint8_t, uint8_t>> compressed;
+    for (const auto& line : bitmap) {
+        for (auto it = line.cbegin(); it != line.cend(); ++it) {
+            auto it2 = it;
+            while (it2 != line.cend()) {
+                int i = 1;
+                if (*it2 == *(it2 + i++)) {
+                    std::advance(it2, 1);
+                } else if (*it2 != *(it2 + i)) {
+                    break;
+                }
+            }
+        }
+    }
+}
