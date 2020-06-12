f941f0cb5b9646020c36bd5ed94d1a167dacf2a3
kyrtaq <kyrtaq13@gmail.com>
2020-06-12 17:45:00 +0200

    compression done

--- module1/homework/grayscaleImages/compression.cpp
+++ module1/homework/grayscaleImages/compression.cpp
@@ -1,19 +1,52 @@
 #include "compression.hpp"
 
+#include <algorithm>
+
 std::vector<std::pair<uint8_t, uint8_t>> compressGrayscale(
     const std::array<std::array<uint8_t, width>, height>& bitmap) {
     std::vector<std::pair<uint8_t, uint8_t>> compressed;
+
+    uint8_t colour = 0;
+    int times = 0;
+
     for (const auto& line : bitmap) {
-        for (auto it = line.cbegin(); it != line.cend(); ++it) {
-            auto it2 = it;
-            while (it2 != line.cend()) {
-                int i = 1;
-                if (*it2 == *(it2 + i++)) {
-                    std::advance(it2, 1);
-                } else if (*it2 != *(it2 + i)) {
-                    break;
-                }
+        colour = line.front();
+        times = 0;
+        for (const auto el : line) {
+            if (el == colour) {
+                ++times;
+            } else {
+                compressed.push_back(std::make_pair(colour, times));
+                times = 1;
+                colour = el;
             }
         }
+        compressed.push_back(std::make_pair(colour, times));
     }
+    return compressed;
+}
+
+std::array<std::array<uint8_t, width>, height> decompressGrayscale(const std::vector<std::pair<uint8_t, uint8_t>>& compressed) {
+    std::array<std::array<uint8_t, width>, height> decompressed;
+
+    size_t row = 0;
+    size_t col = 0;
+
+    for (const auto& [color, count] : compressed) {
+        if (row < height) {
+            for (size_t i = 0; i < count; i++) {
+                decompressed[row][col] = color;
+                if (col < width - 1) {
+                    col++;
+                } else {
+                    col = 0;
+                    row++;
+                }
+            }
+        } else {
+            break;
+        }
+    }
+
+    return decompressed;
 }
